fe424ce192240391c85ec152bc183a7e5962dc6f
Abdul Fatah <abdulfatah66@gmail.com>
2019-06-16 17:10:56 +0500

    removed unnecessary functions

--- src/main/scala/Transcendentals.scala
+++ src/main/scala/Transcendentals.scala
@@ -263,17 +263,6 @@ object Transcendentals {
       }
     })
 
-    def cos2(): sqMatrix = sumTuple((0 until nTerms).foldLeft((identity, this)) {
-      case ((sum, term), n) => n match {
-        case even if n % 2 == 0 => {
-          (sum + term, pow(n) * (1.0 / factorial(n - 1)))
-        }
-        case odd if n % 2 > 0 => {
-          (sum - term * pow(n + 1) * (1 / factorial(n + 1)), pow(n + 2) * (1 / factorial(n + 2)))
-        }
-      }
-    })
-
     //          M^1   M^3   M^5   M^7
     // sin(M) = --- - --- + --- - --- + ...
     //           1!    3!    5!    7!
@@ -281,16 +270,6 @@ object Transcendentals {
       case ((sum, term), n) => n match {
         case even if n % 2 == 0 => (sum + term, pow(n + 2) * (1.0 / factorial(n + 2)))
         case odd if n % 2 > 0 => (sum - term, pow(n + 2) * (1.0 / factorial(n + 2)))
-      }
-    })
-    def sin2(): sqMatrix = sumTuple((1 until nTerms).foldLeft((identity, this)) {
-      case ((sum, term), n) => n match {
-        case even if n % 2 == 0 => {
-          (sum + term.pow(n) * (1.0/factorial(n-1)), this)
-        }
-        case odd if n % 2 > 0 => {
-          (sum - term.pow(n + 3) * (1/factorial(n + 3)), this)
-        }
       }
     })
 
